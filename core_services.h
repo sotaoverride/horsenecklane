d2a8dfd8d8e724e2decf94a2e83c7d26f7ac62fb
Aatir Manzur <aatrapps@gmail.com>
2023-09-16 00:44:01 -0400

    start

---
+++ core_services.h
@@ -0,0 +1,33 @@
+#ifndef CORE_SERVICES_HEADER
+#define CORE_SERVICES_HEADER
+#define QUEUE_SIZE 55
+
+enum Component {temperatre, humidity};
+typedef struct CoreRequest{
+	char dest[4];
+	char org[4];
+	char req[4];
+} CoreRequest;
+
+typedef struct CoreResponse{
+	char dest[4];
+	char org[4];
+	//response
+} CoreResponse;
+
+typedef struct CoreQueue{
+	int head;
+	int tail;
+	union{
+		CoreRequest req_arr[QUEUE_SIZE];
+
+		CoreResponse res_arr[QUEUE_SIZE];
+	};
+} CoreQueue;
+
+typedef struct CoreComp{
+	char* comp_abv;
+	CoreQueue incoming_queue;
+	CoreQueue outgoing_queue;
+} CoreComp;
+#endif
---
+++ product.c
@@ -0,0 +1,5 @@
+#include "core_services.h"
+#include "temperature_component.h"
+int main(){
+	return 0;
+}
